3e95932125dcebb32746d3a151bc8fc5ccf22694
Abenezer Merdassa <abenezermerdassa@gmail.com>
2021-07-21 10:18:08 -0500

    product and category routes

--- models/index.js
+++ models/index.js
@@ -6,31 +6,27 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreginKey: 'tag_id',
+  foreginKey: 'category_id',
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreginKey: 'tag_id'
+  foreginKey: 'category_id',
+  onDelete: 'CASCADE'
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  as: 'product_tag'
-});
-
+   through: {
+     model: ProductTag,
+     uniqe: false
+   }, as: 'product_tag'   });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongToMany(Product, {
   through: {
     model: ProductTag,
-    unique: false
-  },
-  as: 'tagged_product'
-});
+    uniqe: false
+  }, as: 'tag_product' });
 
 module.exports = {
   Product,
